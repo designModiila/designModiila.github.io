4d885213c794fd003c1f20a593494e3d7a89c40f
Pjungju <design@modiila.com>
2022-10-19 10:16:07 +0900

    인터렉션js수정10191016

--- js/test.js
+++ js/test.js
@@ -70,19 +70,40 @@ $(function (){
       TweenMax.staggerFromTo($('header'), 0.5, {opacity: 0}, {opacity: 1, delay:0.3, ease: Power1.easeInOut})
       TweenMax.staggerFromTo($("#fp-nav"), 0.8, {opacity: 0, x: '0%'}, {opacity: 1, x: '0%',delay:0.3, ease: Power1.easeInOut});
 			TweenMax.staggerFromTo($(".scroll-btn"), 0.8, {opacity: 0,}, {opacity: 1, delay:0.5, ease: Power1.easeInOut});
-      TweenMax.staggerFromTo($(".container .title"), 1.2, {opacity: 0, y: 100}, {opacity: 1, y: 0, delay: 0.2, ease: Power1.easeInOut}, 0.05);
-      TweenMax.staggerFromTo($(".container .title span"), 0.8, {opacity:0,y:'120%'}, {opacity: 1, y:'0%', delay: 0.2, ease: Power1.easeInOut}, 0.05);
-      TweenMax.staggerFromTo($(".l-txt-box .txt"), 1.5, {opacity:0, y:'0%'}, {opacity: 1, y:'0%', delay: 1, ease: Power1.easeInOut}, 0.2);
-      TweenMax.staggerFromTo($(".l-txt-box .date"), 1, {opacity:0}, {opacity: 1, delay:1.1, ease: Power1.easeInOut}, 0.2);
+      if(index == 1){
+        textMotion();
+        textMotionInit();
+      }
+      if(index == 2){
+        textMotion();
+        textMotionInit();
+      }
+      if(index == 3){
+        textMotion();
+        textMotionInit();
+      }
+      if(index == 4){
+        textMotion();
+        textMotionInit();
+      }
+
     },
-    onLeave: function(index, nextIndex, direction){
+    onLeave: function(anchorLink, direction){
 			TweenMax.staggerFromTo($("header"), 0.8, {opacity: 0}, {opacity: 0, ease: Power1.easeOut});
       TweenMax.staggerFromTo($("#fp-nav"), 0.8, {opacity: 0, x: '0%'}, {opacity: 0, x: '0%', ease: Power1.easeOut});
 			TweenMax.staggerFromTo($(".scroll-btn"), 0, {opacity: 1}, {opacity: 0,ease: Power1.easeInOut});
-      TweenMax.staggerFromTo($(".container .title"), 0, {opacity:0,y:'0%'}, {opacity: 0, y:'60%', ease: Power1.ease});
-      TweenMax.staggerFromTo($(".container .title span"), 0, {opacity:0,y:'0%'}, {opacity: 0, y:'0%', ease: Power1.easeInOut});
-      TweenMax.staggerFromTo($(".l-txt-box .txt"), 0, {opacity:0, y:'0%'}, {opacity: 0, y:'100%',ease: Power1.easeInOut});
-      TweenMax.staggerFromTo($(".l-txt-box .date"), 0, {opacity:0}, {opacity: 0, ease: Power1.easeInOut});
+      if(anchorLink == 1 && direction =='down'){
+        textMotionInit();
+			}
+      if(anchorLink == 2 && direction =='down'){
+        textMotionInit();
+			}
+      if(anchorLink == 3 && direction =='down'){
+        textMotionInit();
+			}
+      if(anchorLink == 4 && direction =='down'){
+        textMotionInit();
+			}
     }
   });
 });
