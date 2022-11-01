7306e5deff23c1116ba465c6f798c565ed980115
Pjungju <design@modiila.com>
2022-11-01 14:05:34 +0900

    11011405수정

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ $(function (){
       });
 
       // TweenMax.staggerFromTo($(".container p"), 1.2, {opacity: 0, y: 100}, {opacity: 1, y: 0, delay: 0.2, ease: Power1.easeInOut}, 0.05);
-      TweenMax.staggerFromTo($(".container > p > span"), 0.8, {opacity:0, y:'120%'}, {opacity: 1, y:'0%', delay: 0.2, ease: Power1.easeInOut}, 0.05);
+      TweenMax.staggerFromTo($(".container > p > span"), 0.8, {opacity:0, bottom: -60, ease: Back.easeOut.config(1.7)}, {opacity: 1, bottom: 0, ease: Back.easeOut.config(1.7)}, 0.05);
       TweenMax.staggerFromTo($(".l-txt-box .txt"), 1.5, {opacity:0, y:'0%'}, {opacity: 1, y:'0%', delay: 1, ease: Power1.easeInOut}, 0.2);
       TweenMax.staggerFromTo($(".l-txt-box .date"), 1, {opacity:0}, {opacity: 1, delay:1.1, ease: Power1.easeInOut}, 0.2);
     },
