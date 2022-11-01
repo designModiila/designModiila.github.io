4bac371247937b5e02815b11e4ed78508cdb7109
Pjungju <design@modiila.com>
2022-11-01 11:12:14 +0900

    11011112수정

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ $(function () {
 $(function(){
   $.each($(".container p"), function(i, v){
     var mainText = $(this);
-    var mainTextSpace = $(this).text();
+    // var mainTextSpace = $(this).text();
     var animatedMainT = $(this).text().split("");
   
     mainText.empty();
@@ -54,7 +54,7 @@ $(function (){
       TweenMax.staggerFromTo($('header'), 0.5, {opacity: 0}, {opacity: 1, delay:0.3, ease: Power1.easeInOut})
       TweenMax.staggerFromTo($("#fp-nav"), 0.8, {opacity: 0, x: '0%'}, {opacity: 1, x: '0%',delay:0.3, ease: Power1.easeInOut});
 			TweenMax.staggerFromTo($(".scroll-btn"), 0.8, {opacity: 0,}, {opacity: 1, delay:0.5, ease: Power1.easeInOut});
-      // TweenMax.staggerFromTo($(".container p"), 1.2, {opacity: 0, y: 100}, {opacity: 1, y: 0, delay: 0.2, ease: Power1.easeInOut}, 0.05);
+      TweenMax.staggerFromTo($(".container > p"), 1.2, {opacity: 0, y: 100}, {opacity: 1, y: 0, delay: 0.2, ease: Power1.easeInOut}, 0.05);
       TweenMax.staggerFromTo($(".container > p > span"), 0.8, {opacity:0, y:'120%'}, {opacity: 1, y:'0%', delay: 0.2, ease: Power1.easeInOut}, 0.05);
       TweenMax.staggerFromTo($(".l-txt-box .txt"), 1.5, {opacity:0, y:'0%'}, {opacity: 1, y:'0%', delay: 1, ease: Power1.easeInOut}, 0.2);
       TweenMax.staggerFromTo($(".l-txt-box .date"), 1, {opacity:0}, {opacity: 1, delay:1.1, ease: Power1.easeInOut}, 0.2);
@@ -63,7 +63,7 @@ $(function (){
 			TweenMax.staggerFromTo($("header"), 0.8, {opacity: 0}, {opacity: 0, ease: Power1.easeOut});
       TweenMax.staggerFromTo($("#fp-nav"), 0.8, {opacity: 0, x: '0%'}, {opacity: 0, x: '0%', ease: Power1.easeOut});
 			TweenMax.staggerFromTo($(".scroll-btn"), 0, {opacity: 1}, {opacity: 0,ease: Power1.easeInOut});
-      // TweenMax.staggerFromTo($(".container p"), 0, {opacity:0,y:'0%'}, {opacity: 0, y:'60%', ease: Power1.ease});
+      TweenMax.staggerFromTo($(".container > p"), 0, {opacity:0,y:'0%'}, {opacity: 0, y:'60%', ease: Power1.ease});
       TweenMax.staggerFromTo($(".container > p > span"), 0, {opacity:0,y:'0%'}, {opacity: 0, y:'120%', ease: Power1.easeInOut});
       TweenMax.staggerFromTo($(".l-txt-box .txt"), 0, {opacity:0, y:'0%'}, {opacity: 0, y:'100%',ease: Power1.easeInOut});
       TweenMax.staggerFromTo($(".l-txt-box .date"), 0, {opacity:0}, {opacity: 0, ease: Power1.easeInOut});
