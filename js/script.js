aa56ffc6ed08eba8fa3e931952bb4016735784f5
Pjungju <design@modiila.com>
2022-11-08 20:33:59 +0900

    11082033request_label수정

--- js/script.js
+++ js/script.js
@@ -13,26 +13,26 @@ $(function (){
 
   $('.main-title').addClass('on');
 
-  $('.pc_tab li').first().addClass("activeClass");
-  $(".tab-contents").not(':first').hide();
+  // $('.pc_tab li').first().addClass("activeClass");
+  // $(".tab-contents").not(':first').hide();
 
-  $('.pc_tab li').on('click', function () {
-    $(this).addClass("activeClass").siblings().removeClass("activeClass");
-    var link = $(this).find("a").attr("href");
-    var link_num = link.substr(link.length - 1);
-    $("select#tabmenu option").eq(link_num - 1).prop("selected", "selected");
-    $(".tab-contents").hide();
-    $(link).show();
-  });
+  // $('.pc_tab li').on('click', function () {
+  //   $(this).addClass("activeClass").siblings().removeClass("activeClass");
+  //   var link = $(this).find("a").attr("href");
+  //   var link_num = link.substr(link.length - 1);
+  //   $("select#tabmenu option").eq(link_num - 1).prop("selected", "selected");
+  //   $(".tab-contents").hide();
+  //   $(link).show();
+  // });
 
-  $("select#tabmenu").on("change", function () {
-    var select_link = $("select#tabmenu").val();
-    var select_num = $(this).prop('selectedIndex');
-    $('.pc_tab li').eq(select_num).addClass("activeClass").siblings().removeClass('activeClass');
-    $(".tab-contents").hide();
-    $(select_link).show();
-    console.log(select_link);
-  });
+  // $("select#tabmenu").on("change", function () {
+  //   var select_link = $("select#tabmenu").val();
+  //   var select_num = $(this).prop('selectedIndex');
+  //   $('.pc_tab li').eq(select_num).addClass("activeClass").siblings().removeClass('activeClass');
+  //   $(".tab-contents").hide();
+  //   $(select_link).show();
+  //   console.log(select_link);
+  // });
 
   function textMotion(){
     $.each($(".container p"), function(i, v){
