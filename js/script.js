d39b234b4e53219ac07d2fdf140b50a72384ead3
Pjungju <design@modiila.com>
2022-11-11 13:06:03 +0900

    11111305수정

--- js/script.js
+++ js/script.js
@@ -9,30 +9,81 @@ $(function (){
   $('#toggle').click(function () {
     $('#toggle .bar').toggleClass('animate');
     $('#menu-page').toggleClass('overlay');
+    $('body').on('scroll touchmove mousewheel', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+    });
   });
 
   $('.main-title').addClass('on');
 
-  // $('.pc_tab li').first().addClass("activeClass");
-  // $(".tab-contents").not(':first').hide();
+  $(document).on('click','.privacy',function(){
+    $('#pop_bg').fadeIn();
+    $('#foot_popup').fadeIn();
+    $('body').on('scroll touchmove mousewheel', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+    });
+  });
 
-  // $('.pc_tab li').on('click', function () {
-  //   $(this).addClass("activeClass").siblings().removeClass("activeClass");
-  //   var link = $(this).find("a").attr("href");
-  //   var link_num = link.substr(link.length - 1);
-  //   $("select#tabmenu option").eq(link_num - 1).prop("selected", "selected");
-  //   $(".tab-contents").hide();
-  //   $(link).show();
-  // });
 
-  // $("select#tabmenu").on("change", function () {
-  //   var select_link = $("select#tabmenu").val();
-  //   var select_num = $(this).prop('selectedIndex');
-  //   $('.pc_tab li').eq(select_num).addClass("activeClass").siblings().removeClass('activeClass');
-  //   $(".tab-contents").hide();
-  //   $(select_link).show();
-  //   console.log(select_link);
-  // });
+
+  $(document).on('click','.popup-close, #pop_bg',function(){
+    $('#pop_bg').hide();
+    $('#foot_popup').hide();
+  });
+
+  // function popup() {
+
+  //   //레이어팝업
+  //   var bg = $("#bg");
+  
+  //   $(document).on('click',' #bg, .popup-close ', function () {
+  //     bg.fadeOut();
+  //     $('div[id*="-popup"] div.inner').html('');
+  //     $('div[id*="-popup"]').hide();
+  
+  //     $('#foot_popup div.inner').html('');
+  //     $('#foot_popup').hide();
+  
+  //     $('body').off('scroll touchmove mousewheel');
+  //   });
+  
+  //   $(document).on('click','.privacy-btn',
+  //     function () {
+  //     var ClassName = $(this).data('link');
+  
+  //     bg.show();
+  //     $('#foot_popup').show();
+  //     $('#foot_popup').html(
+  //       '<div class="popup-close"><img src="/images/common/popup_close_icon.png" alt="close" class="close"></div>'+
+  //       '<div class="inner"></div>'
+  //     );
+  
+  //     //foot 메뉴 클릭시
+  //     switch (ClassName) {
+  //       case "email1":
+  //         $("#foot_popup div.inner").load("/_email1.php");
+  //       break;
+  //       case "privacy1":
+  //         $("#foot_popup div.inner").load("/_privacy1.php");
+  //       break;
+  //     }
+  
+  //     $('body').on('scroll touchmove mousewheel', function(event) {
+  //       event.preventDefault();
+  //       event.stopPropagation();
+  //       return false;
+  //     });
+  
+  //   }); //click
+  
+  // }
+  
+  // popup();
+
 
   function textMotion(){
     $.each($(".container p"), function(i, v){
@@ -62,10 +113,13 @@ $(function (){
     navigation: true,
     slidesToSections: true,
     scrollingSpeed: 850,
-    afterLoad: function(){
+    afterLoad: function(anchorLink, index, direction){
       TweenMax.staggerFromTo($('header'), 0.5, {opacity: 0}, {opacity: 1, delay:0.3, ease: Power1.easeInOut})
       TweenMax.staggerFromTo($("#fp-nav"), 0.8, {opacity: 0, x: '0%'}, {opacity: 1, x: '0%',delay:0.3, ease: Power1.easeInOut});
 			TweenMax.staggerFromTo($(".scroll-btn"), 0.8, {opacity: 0,}, {opacity: 1, delay:0.5, ease: Power1.easeInOut});
+      if (index == 5){
+        $('.scroll-btn').css('display','none');
+      }
     },
     onLeave: function(){
 			TweenMax.staggerFromTo($("header"), 0.8, {opacity: 0}, {opacity: 0, ease: Power1.easeOut});
@@ -76,4 +130,3 @@ $(function (){
     }
   });
 });
-
